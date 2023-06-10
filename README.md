b94f2659e50e8042a7032cf24566f143af26e789
TonySchilling <tjjpschilling7@gmail.com>
2023-06-09 23:26:58 -0500

    Final Commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ReadingList
-Simple reading list tracker app
+This is a simple app that allows users to create and save a reading list.
+
+Users can add new books to the list, storing the book's title, author, number of pages and whether or not the user has finished reading the book. 
+Users can also save the reading list, making it available in local storage, as well as clear the reading list (which will also remove it from local storage)
+
+This was a sort exercise with the goal of practicing several concepts:
+-Saving/referencing local storage
+-Using Bootstrap
+-Working with objects
+
--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 const readingList = [];
+const libraryEntries = []
+
+const storedData = localStorage.getItem('readingList');
+console.log(storedData);
 
 const newBookButton = document.getElementById("add-new-book");
+const newBookCard = document.getElementById("new-book-card");
+const saveListButton = document.getElementById("save-list");
+const clearListButton = document.getElementById("clear-list");
 const myLibrary = document.getElementById("library-container");
 const addBookButton = document.getElementById('add-book');
+const cancelButton = document.getElementById('cancel-btn');
+// const cancelButton = document.getElementById('alerts-section');
+
+
+
+saveListButton.addEventListener("click", () => {
+    showAlert('Reading List saved. You can access it any time!');
+    updateStoredData(readingList);    
+})
+
+clearListButton.addEventListener("click", () => {
+    showAlert('Reading List cleared...')
+    deleteCards();
+    updateStoredData([]);    
+})
 
 function bookObject(title, author, pages, read) {
     this.title = title
@@ -12,34 +34,65 @@ function bookObject(title, author, pages, read) {
 
 }
 
+function updateStoredData(data) {
+    const jsonData = JSON.stringify(data);
+    localStorage.setItem('readingList', jsonData);
+    console.log(localStorage.getItem('readingList'));
+}
+
 addBookButton.addEventListener("click", () => {
 
     // const bookCard = document.getElementById('new-book-card');
-    const title = document.getElementById('new-title').value;
-    const author = document.getElementById('new-author').value;
-    const pages = document.getElementById('new-pages').value;
-    const read = document.getElementById('exampleCheck1').checked;
+    const title = document.getElementById('new-title');
+    const author = document.getElementById('new-author');
+    const pages = document.getElementById('new-pages');
+    const read = document.getElementById('exampleCheck1');
     console.log(read);
 
-    const newEntry = new bookObject(title, author, pages, read);
+    const newEntry = new bookObject(title.value, author.value, pages.value, read.checked);
     console.log(newEntry);
 
     readingList.push(newEntry);
-    createNewCard(title, author, pages, read, myLibrary);
+    // const jsonData = JSON.stringify(readingList);
+    // localStorage.setItem('readingList', jsonData)
+    createNewCard(title.value, author.value, pages.value, read.checked, myLibrary);
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    read.checked = false;
 
     
 })
 
+cancelButton.addEventListener("click", () => {
+    const form = document.getElementById("new-book-card");
+    form.style.display = "none";
+
+})
+
 const newOne = new bookObject('wrwer', 'werwer', 23, false);
 console.log(newOne);
+
 newBookButton.addEventListener("click", () => {
+    
     addBook();
+    hideCards();
+
 })
 
 function addBook() {
     const form = document.getElementById("new-book-card");
     form.style.display = "block";
     console.log(form.style);
+}
+
+function updateReadingList(removedTitle) {
+
+    readingList.forEach((book, index) => {
+        if (book.title === removedTitle) {
+          readingList.splice(index, 1);
+        }
+      });
 }
 
 
@@ -79,6 +132,7 @@ function createNewCard(newTitle, newAuthor, newPages, newRead, parent) {
     const mainHeader = newElement('h5', null, 'card-title', null, null, cardBody);
     mainHeader.textContent = newTitle;
     const bookDetails = newElement('div', null, 'book-details', null, null, cardBody);
+    bookDetails.style.display = "none";
 
     const authorTitle = newElement('h6', null, 'card-title', null, null, bookDetails);
     authorTitle.textContent = 'Author: ';
@@ -102,19 +156,106 @@ function createNewCard(newTitle, newAuthor, newPages, newRead, parent) {
 
     const removeButton = document.createElement('a');
     removeButton.href = '#';
-    removeButton.className = 'btn btn-primary';
+    removeButton.className = 'btn btn-secondary';
     removeButton.textContent = 'Remove';
-    cardBody.appendChild(removeButton);
+    bookDetails.appendChild(removeButton);
+
+    removeButton.addEventListener("click", () => {
+        parent.removeChild(newCard);
+        updateReadingList(newTitle);
+    })
 
     parent.appendChild(newCard);
+
+    newCard.addEventListener("click", () => {
+        const form = document.getElementById("new-book-card");
+        form.style.display = "none";
+        selectCard(newCard);
+
+    })
+    
 
 
 }
 
 
-// createNewCard('Prisoner of Aks', 'JK ROwling', '115', true, myLibrary);
+function hideCards() {
+    const books = document.getElementsByClassName('card');
+
+    for (let i = 0; i < books.length; i++) {
+        const element = books[i];
+        console.log(element);
+        if (element.id !== "new-book-card" && element.id !== "library-container") {
+            const cardDetails = element.getElementsByClassName("book-details")[0];
+            cardDetails.style.display = "none";
+            console.log(cardDetails);
+        }
+      }
+      
+}
+
+function deleteCards() {
+    const books = document.getElementsByClassName('card');
+    const booksArray = Array.from(books);
+
+    booksArray.forEach((element, index) => {
+        if (element.id !== "new-book-card" && element.id !== "library-container") {
+            myLibrary.removeChild(element);
+        }
+    })
+
+    // for (let i = 0; i < books.length; i++) {
+    //     const element = books[i];
+    //     if (element.id !== "new-book-card" && element.id !== "library-container") {
+    //         myLibrary.removeChild(books[i]);
+    //     }
+    //   }
+      
+}
+
+function showAlert(alertMessage) {
+
+    const alertsSection = document.getElementById('alerts-section');
+    alertsSection.innerHTML ='';
+    const alert = document.createElement('div');
+    alert.className = "alert alert-primary alert-dismissible fade show";
+    alert.role = "alert"
+    const alertText = document.createElement('strong');
+    alertText.textContent = alertMessage;
+    alert.appendChild(alertText);
+    const dismissButton = document.createElement('button');
+    dismissButton.type = 'button';
+    dismissButton.className = 'btn-close';
+    dismissButton.setAttribute("data-bs-dismiss", "alert");
+    dismissButton.setAttribute("aria-label", "Close");
+    alert.appendChild(dismissButton);
+
+    alertsSection.appendChild(alert);
+
+}
 
 
-// const newCard = newElement('div', null, 'card', "width: 18rem;", null, myLibrary);
+function selectCard(card) {
+    const books = document.getElementsByClassName('card');
 
-// const cardBody = newElement('div', null, 'card-body', null, null, newCard);
+    hideCards();
+
+    const activeDetails = card.getElementsByClassName("book-details")[0];
+    activeDetails.style.display = "block";
+    console.log(activeDetails);
+
+      
+}
+
+
+try {
+    const storedReadingList = JSON.parse(storedData);
+
+    storedReadingList.forEach((item, index) => {
+        readingList.push(item);
+        createNewCard(item.title, item.author, item.pages, item.read, myLibrary)
+    })
+} catch {
+    console.log('Didnt work');
+}
+
